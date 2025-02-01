e90d7a89e020f200a8c1ed52011e63fd94bd10c4
Stream <1542763342@qq.com>
2025-02-01 17:41:16 +0800

    feat: add shadow plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("buildsrc.convention.kotlin-jvm")
     application
     alias(libs.plugins.kotlinx.serialization)
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 dependencies {
