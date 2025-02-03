c2a4c61767da262b3911810fef71783ccb0ab50f
Stream <1542763342@qq.com>
2025-02-03 13:16:31 +0800

    refactor: change to use deepSeek V3

--- app/src/main/kotlin/io/github/stream29/agent/App.kt
+++ app/src/main/kotlin/io/github/stream29/agent/App.kt
@@ -4,7 +4,7 @@ import kotlinx.coroutines.runBlocking
 
 fun main() {
     runBlocking {
-        val simpleAgent = SimpleAgent(qwenChatApiProvider, Status())
+        val simpleAgent = SimpleAgent(deepSeekChatApiProvider, Status())
         simpleAgent.query("请介绍一下中国的MC玩家群体")
     }
 }
--- app/src/main/kotlin/io/github/stream29/agent/ChatApi.kt
+++ app/src/main/kotlin/io/github/stream29/agent/ChatApi.kt
@@ -1,14 +1,25 @@
 package io.github.stream29.agent
 
 import dev.langchain4j.model.dashscope.QwenChatModel
+import dev.langchain4j.model.openai.OpenAiChatModel
 import io.github.stream29.langchain4kt.api.langchain4j.asChatApiProvider
 
-val apiKey = System.getenv("ALIBABA_QWEN_API_KEY") ?: throw RuntimeException("api key not found")
+val qwenApiKey = System.getenv("ALIBABA_QWEN_API_KEY") ?: throw RuntimeException("api key not found")
 
 val qwenChatApiProvider =
     QwenChatModel.builder()
-        .apiKey(apiKey)
+        .apiKey(qwenApiKey)
         .modelName("qwen-max-latest")
         .temperature(1.0f)
+        .build()
+        .asChatApiProvider()
+
+val deepSeekApiKey = System.getenv("DEEPSEEK_API_KEY") ?: throw RuntimeException("api key not found")
+
+val deepSeekChatApiProvider =
+    OpenAiChatModel.builder()
+        .baseUrl("https://api.deepseek.com/v1")
+        .apiKey(deepSeekApiKey)
+        .modelName("deepseek-chat")
         .build()
         .asChatApiProvider()
