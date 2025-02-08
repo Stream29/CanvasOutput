aa438a2ea1056ade2b0a7f74f3fb3c125ca10fc0
Stream <1542763342@qq.com>
2025-02-09 00:26:16 +0800

    feat: more and better api

--- app/src/main/kotlin/io/github/stream29/agent/ChatApi.kt
+++ app/src/main/kotlin/io/github/stream29/agent/ChatApi.kt
@@ -2,14 +2,16 @@ package io.github.stream29.agent
 
 import dev.langchain4j.model.dashscope.QwenChatModel
 import dev.langchain4j.model.openai.OpenAiChatModel
+import io.github.stream29.langchain4kt.api.googlegemini.GeminiChatApiProvider
 import io.github.stream29.langchain4kt.api.langchain4j.asChatApiProvider
+import java.time.Duration
 
 val qwenApiKey = System.getenv("ALIBABA_QWEN_API_KEY") ?: throw RuntimeException("api key not found")
 
 val qwenChatApiProvider =
     QwenChatModel.builder()
         .apiKey(qwenApiKey)
-        .modelName("qwen-plus-latest")
+        .modelName("qwen-max-latest")
         .temperature(1.0f)
         .build()
         .asChatApiProvider()
@@ -21,5 +23,14 @@ val deepSeekChatApiProvider =
         .baseUrl("https://dashscope.aliyuncs.com/compatible-mode/v1")
         .apiKey(qwenApiKey)
         .modelName("deepseek-v3")
+        .timeout(Duration.ofSeconds(60))
         .build()
         .asChatApiProvider()
+
+val geminiApiKey = System.getenv("GOOGLE_AI_GEMINI_API_KEY") ?: throw RuntimeException("api key not found")
+
+val geminiChatApiProvider =
+    GeminiChatApiProvider(
+        modelName = "gemini-2.0-flash",
+        apiKey = geminiApiKey
+    )
